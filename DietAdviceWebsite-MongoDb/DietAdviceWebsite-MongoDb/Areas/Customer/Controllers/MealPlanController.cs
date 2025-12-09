4b2607f2d85d95daa55fde4b1f261c7ffc4fdf3c
TranLamNghia <tranlam1142004@gmail.com>
2025-12-09 12:23:05 +0700

    feat: add side meal

--- DietAdviceWebsite-MongoDb/DietAdviceWebsite-MongoDb/Areas/Customer/Controllers/MealPlanController.cs
+++ DietAdviceWebsite-MongoDb/DietAdviceWebsite-MongoDb/Areas/Customer/Controllers/MealPlanController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using DietAdviceWebsite_MongoDb.Areas.Customer.Services;
+using DietAdviceWebsite_MongoDb.Areas.Customer.ViewModels;
 using DietAdviceWebsite_MongoDb.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -22,15 +23,39 @@ namespace DietAdviceWebsite_MongoDb.Areas.Customer.Controllers
         [Route("customer/meal-plan/index")]
         public async Task<IActionResult> Index()
         {
-            var mealPlan = await _mealPlanService.GetTodayMealPlanAsync();
+            var mealPlan = await _mealPlanService.GetTodayMealPlanViewModelAsync();
             return View(mealPlan);
         }
 
-        [HttpGet("GetAllMeals")]
+        [HttpGet]
+        [Route("customer/meal-plan/get-all-meals")]
         public async Task<JsonResult> GetAllMeals()
         {
             List<Meal> listMeals = await _mealManagementService.GetAsync(null, null);
             return Json(listMeals);
+        }
+
+        [HttpPost]
+        [Route("customer/meal-plan/add-meal")]
+        public async Task<IActionResult> AddMeal(string MealId, string TimeSlot, double Quantity, string Unit) 
+        {
+            if (!ModelState.IsValid)
+            {
+                return Json(new { success = false, message = "Invalid data" });
+            }
+
+            try
+            {
+                await _mealPlanService.AddMealToPlanAsync(MealId, TimeSlot, Quantity, Unit);
+
+                var updatedMealPlan = await _mealPlanService.GetTodayMealPlanViewModelAsync();
+                return Json(new { success = true, data = updatedMealPlan });
+            }
+            catch (System.Exception ex)
+            {
+                // Log the exception
+                return StatusCode(500, new { success = false, message = ex.Message });
+            }
         }
     }
 }
---
+++ DietAdviceWebsite-MongoDb/DietAdviceWebsite-MongoDb/Areas/Customer/ViewModels/AddMealViewModel.cs
@@ -0,0 +1,10 @@
+namespace DietAdviceWebsite_MongoDb.Areas.Customer.ViewModels
+{
+    public class AddMealViewModel
+    {
+        public string MealId { get; set; }
+        public double Quantity { get; set; }
+        public string Unit { get; set; }
+        public string TimeSlot { get; set; }
+    }
+}
